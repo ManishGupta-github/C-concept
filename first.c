880c43105037e62b8d2017603dd5862f21b2f96d
manish <manishgupta23956@gmail.com>
2025-07-29 18:43:08 +0530

    new change

---
+++ first.c
@@ -0,0 +1,37 @@
+// write printf statement to print following on the screen
+// consider->
+// 1. 5  6
+// 2.a=5 and b=6
+// 3.a=6 & b=5
+// value of a is 5 and value of b is 6
+// sum of 5 and 6 is 11
+// 5+6=11
+#include <stdio.h>
+// int a=5,b=6;
+int main()
+{
+  //     printf("%d %d",a,b);
+  //     printf("\n");
+  //     printf("a=%d and b=%d",a,b);
+  //     printf("\n");
+  //     printf("a=%d & b=%d",b,a);
+  //     printf("\n");
+  //     printf("value of a is %d and value of b is %d",a,b);
+  //     printf("\n");
+  //     printf("sum of %d and %d is %d",a,b,a+b);
+  //     printf("\n");
+  //     printf("%d+%d=%d",a,b,a+b);
+  // User Input
+  // int a;
+  // scanf("%d",&a);
+  // printf("%d",a);
+  // write program to calculate sum of two integers
+  // int a,b;
+  // scanf("%d %d",&a,&b);
+  // printf("The sum of %d and %d is %d",a,b,a+b);
+  int hour, minute;
+  printf("Enter Time:");
+  scanf("%d:%d", &hour, &minute);
+  printf("%d Hour and %d Minute",hour,minute);
+  return 0;
+}
