bc40433de1723106d1ffc955c4c6ab1a5c7ff0d2
manish <manishgupta23956@gmail.com>
2025-08-19 22:53:06 +0530

    commit4

--- first.c
+++ first.c
@@ -1,12 +1,12 @@
-//gcc first.c -o first.exe
-// write printf statement to print following on the screen
-// consider->
-// 1. 5  6
-// 2.a=5 and b=6
-// 3.a=6 & b=5
-// value of a is 5 and value of b is 6
-// sum of 5 and 6 is 11
-// 5+6=11
+// gcc first.c -o first.exe
+//  write printf statement to print following on the screen
+//  consider->
+//  1. 5  6
+//  2.a=5 and b=6
+//  3.a=6 & b=5
+//  value of a is 5 and value of b is 6
+//  sum of 5 and 6 is 11
+//  5+6=11
 #include <stdio.h>
 // int a=5,b=6;
 int main()
@@ -36,76 +36,77 @@ int main()
   // printf("%d Hour and %d Minute",hour,minute);
   // return 0;
   ; // printf("%lu",sizeof(char));
-  // write program to input number from the user and also input digit.append digit
-  //  the number and print the resulting number.
-  //  (Example,number=234 and digit=9 then resulting number is 2349)
-  //  printf("Enter the first number:");
-  //  int g;
-  //  scanf("%d",&g);
-  //  printf("Enter the Second number:");
-  //  int h;
-  //  scanf("%d",&h);
-  //  int f=g*10+h;
-  //  printf("Resulting number is:%d",f);
-  // write program to take three digit number from the user and rotate its digit by one position
-  // towards the right
-  // printf("Enter the three digit number:");
-  // int temp, temp1, temp2, temp4, temp6;
-  // scanf("%d", &temp); // 123
-  // temp4 = temp % 10;  // 3
-  // temp1 = temp / 10;  // 12
-  // temp2 = temp4 * 100;
-  // temp6 = temp2 + temp1;
-  // printf("%d", temp6); // 312
-//write program to print the first 10 natural number
-// int i=1,n;
-// printf("Enter the number");
-// scanf("%d",&n);
-// while (i<=n)
-// {
-//   printf("%d",i);
-//   printf("\n");
-//   i++;
-// }
-// write program to print first n natural numbers
-// int i=1,n;
-// printf("Enter the Number:");
-// // printf("\n");
-// scanf("%d",&n);
-// while(i<=n)
-// {
-//   printf("%d",i);
-//   printf("\n");
-//   i++;
-// }
-//write program to print first 10 odd natural numbers
-// int i;
-// for(int i=1;i<=10;i++)
-// {
-//   printf("%d ",2*i-1);
-// }
- int j;
-// for(int i=1;i<=5;i++)
-// {
-//   printf("*");
-// }
-// printf("\n");
-//***** 
-// for(j=1; j<=5; j++)
-// {
-//     printf("*\n");
-// }
-//*****
-//*****
-//*****
-// for(int j=1;j<=3;j++)
-// {
-//   for(int i=1;i<=5;i++)
-//   {
-//   printf("*");
-//   }
-//   printf("\n");
-// }
-return 0;
+    // write program to input number from the user and also input digit.append digit
+    //  the number and print the resulting number.
+    //  (Example,number=234 and digit=9 then resulting number is 2349)
+    //  printf("Enter the first number:");
+    //  int g;
+    //  scanf("%d",&g);
+    //  printf("Enter the Second number:");
+    //  int h;
+    //  scanf("%d",&h);
+    //  int f=g*10+h;
+    //  printf("Resulting number is:%d",f);
+    // write program to take three digit number from the user and rotate its digit by one position
+    // towards the right
+    // printf("Enter the three digit number:");
+    // int temp, temp1, temp2, temp4, temp6;
+    // scanf("%d", &temp); // 123
+    // temp4 = temp % 10;  // 3
+    // temp1 = temp / 10;  // 12
+    // temp2 = temp4 * 100;
+    // temp6 = temp2 + temp1;
+    // printf("%d", temp6); // 312
+    // write program to print the first 10 natural number
+  //  int i=1,n;
+  //  printf("Enter the number");
+  //  scanf("%d",&n);
+  //  while (i<=n)
+  //  {
+  //    printf("%d",i);
+  //    printf("\n");
+  //    i++;
+  //  }
+  //  write program to print first n natural numbers
+  //  int i=1,n;
+  //  printf("Enter the Number:");
+  //  // printf("\n");
+  //  scanf("%d",&n);
+  //  while(i<=n)
+  //  {
+  //    printf("%d",i);
+  //    printf("\n");
+  //    i++;
+  //  }
+  // write program to print first 10 odd natural numbers
+  //  int i;
+  //  for(int i=1;i<=10;i++)
+  //  {
+  //    printf("%d ",2*i-1);
+  //  }
+  int j;
+  // for(int i=1;i<=5;i++)
+  // {
+  //   printf("*");
+  // }
+  // printf("\n");
+  //*****
+  // for(j=1; j<=5; j++)
+  // {
+  //     printf("*\n");
+  // }
+  //*****
+  //*****
+  //*****
+  // for(int j=1;j<=3;j++)
+  // {
+  //   for(int i=1;i<=5;i++)
+  //   {
+  //   printf("*");
+  //   }
+  //   printf("\n");
+  // }
+  printf("*");
 
+  return 0;
 }
