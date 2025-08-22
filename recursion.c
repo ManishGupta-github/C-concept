326ff2bd7807fd7f4b01d6f74ef1d0c780ef61c2
manish <manishgupta23956@gmail.com>
2025-08-22 18:45:28 +0530

    Added function prototype

--- recursion.c
+++ recursion.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+int sums(int);
+int main()
+{
+    int k = sums(2);
+    printf("%d", k);
+    return 0;
+}
 int sums(int n)
 {
     if (n == 1)
         return 1;
     int s = sums(n - 1) + n;
     return s;
-}
-int main()
-{
-    int k = sums(2);
-    printf("%d", k);
-    return 0;
 }
